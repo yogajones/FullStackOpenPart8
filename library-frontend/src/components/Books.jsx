3b1d74c442e9e3b2821c8b79d0e7f866206ad3a1
yogajones <95644662+yogajones@users.noreply.github.com>
2024-07-29 13:28:12 +0300

    Ex 8.9

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,9 +1,19 @@
+import { useQuery } from "@apollo/client"
+import { ALL_BOOKS } from "../queries"
+
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+
+  // eslint-disable-next-line react/prop-types
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const books = result.data.allBooks
 
   return (
     <div>
--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -3,10 +3,20 @@ import { gql } from '@apollo/client'
 
 export const ALL_AUTHORS = gql`
   query {
-    allAuthors  {
+    allAuthors {
       name
       born
       bookCount
+    }
+  }
+`
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
     }
   }
 `
